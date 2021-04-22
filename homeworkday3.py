02fb5b9eb988ccc17196dff55f337c6905620ed7
apollojohnson <81492365+apollojohnson@users.noreply.github.com>
2021-04-21 22:59:55 -0500

    Add files via upload

---
+++ homeworkday3.py
@@ -0,0 +1,16 @@
+import math
+
+# house
+L = float(input("Length: "))
+W = float(input("Width: "))
+print("-----")
+print("The area is: ")
+print(L*W)
+print("-----")
+
+# circle
+r = float(input("Radius: "))
+print("-----")
+print("The circumference is: ")
+print(math.pi*r)
+print("-----")
